7b0c202b714fc9f17715b9bab483092531ac8ddf
Vinu Ihalagamage <vinu.i2017@gmail.com>
2024-03-17 18:48:19 -0700

    ocnnecting to DB

--- register.php
+++ register.php
@@ -1,20 +1,20 @@
 <?php
 
-$host = "cosc360.ok.ubc.ca";
-$dbname = 'db_47130992';
-$dbuser = '47130992';
-$dbpass = 'Yarvp04117.';
+// $host = "cosc360.ok.ubc.ca";
+// $dbname = 'db_47130992';
+// $dbuser = '47130992';
+// $dbpass = 'Yarvp04117.';
 
-// $servername = "localhost";
-// $username = "root";
-// $password = "";
-// $dbname = "db_47130992";
+$servername = "cosc360.ok.ubc.ca";
+$username = "47130992";
+$password = "Yarvp04117.";
+$dbname = "db_47130992";
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     // making a connection to the db
-    // $conn = new mysqli($servername, $username, $password, $dbname);
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $dbuser, $dbpass);
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // $pdo = new PDO("mysql:host=$host;dbname=$dbname", $dbuser, $dbpass);
 
     // form data
     $firstName = $_POST['firstname'];
@@ -34,11 +34,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // SQL statements to insert the data
     $stmt = $conn->prepare('INSERT INTO user_details(first_name, last_name, email, username, password) VALUES (?, ?, ?, ?, ?)');
 
-    // $stmt->bind_param('sssss', $firstName, $lastName, $email, $username, $hashedPassword);
+    $stmt->bind_param('sssss', $firstName, $lastName, $email, $username, $hashedPassword);
 
-    // $stmt->execute();
+    $stmt->execute();
 
-    $stmt->execute([$firstName, $lastName, $email, $username, $hashedPassword]);
+    // $stmt->execute([$firstName, $lastName, $email, $username, $hashedPassword]);
 
     //redirect the user to the login page
     header('Location: login.html');
