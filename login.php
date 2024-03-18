ff1d0baf8197f76ec3be7e1c6d84289c8f13b1fe
Vinu Ihalagamage <vinu.i2017@gmail.com>
2024-03-18 01:10:53 -0700

    php for login page (still in progress)

---
+++ login.php
@@ -0,0 +1,60 @@
+<?php
+session_start();
+///// SERVER STUFF /////
+// $host = "cosc360.ok.ubc.ca";
+// $dbname = 'db_47130992';
+// $dbuser = '47130992';
+// $dbpass = 'Yarvp04117.';
+
+// $servername = "cosc360.ok.ubc.ca";
+// $username = "47130992";
+// $password = "Yarvp04117.";
+// $dbname = "db_47130992";
+$servername = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "db_47130992";
+
+// Create a new DB connection
+$db = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+
+// form data
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// Prepare a select statement
+$stmt = $db->prepare("SELECT * FROM users WHERE username = ?");
+$stmt->bind_param("s", $username);
+$stmt->execute();
+$result = $stmt->get_result();
+
+// if the user exists
+if ($result->num_rows > 0) {
+    // fetching the user data
+    $user = $result->fetch_assoc();
+
+    if (password_verify($password, $user['password'])) {
+        // start a session unique to the user and store their username
+        
+        $_SESSION['username'] = $username;
+        $_SESSION['user_id'] = $user['user_id'];
+
+        header('Location: main_page.html');
+        exit();
+    } else {
+        // storing an error message in the session and this can be displayed in the login HTML file
+        
+        $_SESSION['error'] = 'Incorrect password';
+
+        header('Location: login.html');
+        exit();
+    }
+} else {
+    
+    $_SESSION['error'] = 'User does not exist';
+
+    header('Location: login.html');
+    exit();
+}
+
+?>
