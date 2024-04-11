1dce618e133afa16d8a1eab58583831f4001623a
Vinu Ihalagamage <vinu.i2017@gmail.com>
2024-04-11 16:31:59 -0700

    css

--- adminPanel.php
+++ adminPanel.php
@@ -231,12 +231,22 @@ $posts = $conn->query("SELECT postID, title, userID, title, content FROM posts")
                 data: {
                     labels: interactionData.map(item => item.week),
                     datasets: [{
-                        label: 'Weekly Interactions',
+                        label: 'Weekly Comments',
                         data: interactionData.map(item => item.comment_count),
                         backgroundColor: 'rgba(255, 99, 132, 0.5)',
                         borderColor: 'rgba(255, 99, 132, 1)',
                         borderWidth: 1
-                    }]
+                    },
+                    {
+                        label: 'Weekly Posts',
+                        data: interactionData.map(item => item.post_count),
+                        backgroundColor: 'rgba(54, 162, 235, 0.5)',
+                        borderColor: 'rgba(54, 162, 235, 1)',
+                        borderWidth: 1
+                    }
+
+
+                    ]
                 },
                 options: {
                     scales: {
